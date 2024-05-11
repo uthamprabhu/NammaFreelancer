6b028f55796b9c9a6d71b1ef6e57fabbdaf773ff
uthamprabhu <uthamprabhu1@gmail.com>
2024-05-11 18:36:28 +0530

    implemented sidebar and navbar

--- types/index.d.ts
+++ types/index.d.ts
@@ -16,8 +16,32 @@ declare type Account = {
     subtype: string;
     appwriteItemId: string;
     sharableId: string;
-  };
+};
+
+declare type User = {
+    $id: string;
+    email: string;
+    userId: string;
+    dwollaCustomerUrl: string;
+    dwollaCustomerId: string;
+    firstName: string;
+    lastName: string;
+    address1: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    dateOfBirth: string;
+    ssn: string;
+};
 
 declare interface DoughnutChartProps {
     accounts: Account[];
+}
+
+declare interface MobileNavProps {
+    user: User;
+}
+
+declare interface SiderbarProps {
+    user: User;
 }
